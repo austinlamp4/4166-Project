f7c4d7844814b8451ba86fa2049ae741f0c2acff
austinlamp4 <austinlamp4@gmail.com>
2021-11-19 16:48:50 -0500

    Adding boiler-plate flash messages + integrating persistent sessions (for up to 24 hours).

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const methodOverride = require('method-override');
 const connectionRoutes = require('./routes/connectionRoutes');
 const generalRoutes = require('./routes/generalRoutes');
 const mongoose = require('mongoose');
+const flash = require('connect-flash');
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
 
 //create app
 const app = express();
@@ -30,6 +33,24 @@ app.use(express.static('public'));
 app.use(express.urlencoded({extended: true}));
 app.use(morgan('tiny'));
 app.use(methodOverride('_method'));
+
+app.use(session({
+    secret: 'jasbasfibhdjfb',
+    resave: false,
+    saveUninitialized: false, //false -- if session has no data, doesn't store session and not sent to client side
+    cookie:{maxAge: 60*60*1000}, //Cookie that lasts 1 hour (60 sec * 60 min * 1000 millisec/sec)
+    store: new MongoStore({mongoUrl: 'mongodb://localhost:27017/appdevproject'}) //can set an option name for collection to be named something other than sessions
+}));
+
+//Remember, flash after session middleware.
+app.use(flash());
+
+app.use((req, res, next) => {
+    console.log(req.session);
+    res.locals.successMessages = req.flash('success');
+    res.locals.errorMessages = req.flash('error');
+    next();
+});
 
 //set up routes
 app.get('/', (req, res)=> {
