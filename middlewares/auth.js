158884491a1a42cbbaf4d358a17f15e5c6426501
austinlamp4 <austinlamp4@gmail.com>
2021-11-19 22:01:40 -0500

    Added comments to code + authentication module middleware (without implementation into routes).

---
+++ middlewares/auth.js
@@ -0,0 +1,44 @@
+/*
+Author: Austin Lamp
+Date: 11/19/21
+Description: Used within routes module as additional middleware to ensure that a user is authenticated before they are authorized to access certain resources
+within the web server.
+*/
+const Story = require('../models/story');
+
+exports.isGuest = (req, res, next) => {
+    if(!req.session.user) {
+        return next();
+    } else {
+        req.flash('error', 'You are already logged in!');
+        return res.redirect('/users/profile')
+    }
+};
+
+//Make sure user is authenticated
+exports.isLoggedIn = (req, res, next) => {
+    if(req.session.user) {
+        return next();
+    } else {
+        req.flash('error', 'You need to login!');
+        return res.redirect('/users/login')
+    }
+};
+
+//Check if the user is the author of a particular story
+exports.isAuthor = (req, res, next) => {
+    let id = req.params.id;
+    Story.findById(id)
+    .then(story => {
+        if(story) {
+            if(story.author == req.session.user) {
+                return next();
+            } else {
+                let err = new Error('Unauthorized to access the resource');
+                err.status = 401;
+                return next(err);
+            }
+        }
+    })
+    .catch(err => next(err));
+};
--- models/user.js
+++ models/user.js
@@ -1,3 +1,10 @@
+/*
+Author: Austin Lamp
+Date: 11/18/21
+Description: Create a User template with a schema to store users in the database. Functionalities include hashing a password and comparing a plaintext password
+in a given form to the hashed password using bcrypt. The Schema is also used to define what a user should look like.
+*/
+
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const bcrypt = require('bcrypt');
@@ -9,7 +16,7 @@ const userSchema = new Schema({
     password: {type: String, required: [true, 'Cannot be empty']}
 });
 
-
+//Hashes password and saves user.
 userSchema.pre('save', function(next) {
     let user = this; 
     if(!user.isModified('password')) { 
@@ -24,6 +31,8 @@ userSchema.pre('save', function(next) {
     }
 });
 
+
+//Compare plaintext password to hashed password.
 userSchema.methods.comparePassword = function(loginPassword) { 
     return bcrypt.compare(loginPassword, this.password); 
 }
